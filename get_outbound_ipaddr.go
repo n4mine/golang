118c5fe224321df3ee6c96afc760ce81cc7c16b2
n4mine <n4mine@foxmail.com>
2017-05-26 17:27:49 +0800

    add: get outboud ipaddr

---
+++ get_outbound_ipaddr.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net"
+	"strings"
+)
+
+// getOutboundIP Get preferred outbound ip of this machine
+func getOutboundIP() string {
+	conn, err := net.Dial("udp4", "1.2.3.4:56")
+	if err != nil {
+		return ""
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().String()
+	ipPort := strings.Split(localAddr, ":")
+	if len(ipPort) == 2 {
+		return ipPort[0]
+	}
+	return ""
+}
---
+++ get_outbound_ipaddr_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"net"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_getOutboundIP(t *testing.T) {
+	ip := getOutboundIP()
+	assert.NotZero(t, ip)
+	assert.NotNil(t, net.ParseIP(ip))
+}
