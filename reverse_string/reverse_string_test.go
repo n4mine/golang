4d70c23e13e42ef3609dd79c221e95cd6fc76254
n4mine <n4mine@foxmail.com>
2017-05-26 17:51:13 +0800

    add: reverse a string

---
+++ reverse_string/reverse_string_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_reverse(t *testing.T) {
+	assert.Equal(t, "", reverse(""))
+	assert.Equal(t, "1", reverse("1"))
+	assert.Equal(t, "21", reverse("12"))
+	assert.Equal(t, "321", reverse("123"))
+}
