4834e27ccb723e7747010b4f0fea43c6d13a60e3
n4mine <n4mine@foxmail.com>
2017-05-26 13:26:06 +0800

    add: split num

---
+++ split_num.go
@@ -0,0 +1,24 @@
+package main
+
+// split m per n
+func splitN(m, n int) [][]int {
+	var res [][]int
+
+	if n <= 0 {
+		return [][]int{[]int{0, m}}
+	}
+
+	for i := 0; i < m; i = i + n {
+		var start, end int
+		start = i
+		end = i + n
+
+		if end >= m {
+			end = m
+		}
+
+		res = append(res, []int{start, end})
+
+	}
+	return res
+}
